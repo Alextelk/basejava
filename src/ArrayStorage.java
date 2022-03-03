45a22890d098b9696e450a0efaa85e7746318e3a
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-03-03 22:05:41 +0300

    lesson 2 HW

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,35 +4,59 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int count = 0;
     private int size = 0;
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[5];
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
+    private boolean checkUuid(String uuid) {
+        for (count = 0; count < size; count++) {
+            if (storage[count].toString().equals(uuid)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void save(Resume r) {
-        storage[size] = r;
-        size++;
+        if (checkUuid(r.toString())) {
+            System.out.println("Резюме с таким uuid уже есть");
+        } else if (size == storage.length) {
+            System.out.println("Хранилище заполнено");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public void update(Resume r) {
+        if (checkUuid(r.toString())) {
+            storage[count] = r;
+        } else {
+            System.out.println("Резюме с таким uuid в хранилище нет");
+        }
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                return storage[i];
-            }
+        if (checkUuid(uuid)) {
+            return storage[count];
         }
+        System.out.println("Резюме с таким uuid в хранилище нет");
         return null;
     }
 
+
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-            }
+        if (checkUuid(uuid)) {
+            storage[count] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("Резюме с таким uuid в хранилище нет");
         }
     }
 
