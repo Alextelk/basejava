ae81b8aadc8c3160818fc145a4434cb7fc0c1193
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-09-29 14:30:29 +0300

    home work lesson 11

---
+++ src/com/urise/webapp/MainDeadlock.java
@@ -0,0 +1,30 @@
+package com.urise.webapp;
+
+public class MainDeadlock {
+    public static void main(String[] args) {
+        Object o = new Object();
+        Object o1 = new Object();
+        new Thread(() -> {
+            System.out.println("The thread " + Thread.currentThread().getName() + " is running");
+            deadlock(o, o1);
+        }).start();
+        new Thread(() -> {
+            System.out.println("The thread " + Thread.currentThread().getName() + " is running");
+            deadlock(o1, o);
+        }).start();
+    }
+
+    public static void deadlock(Object o, Object o1) {
+        System.out.println("Thread " + Thread.currentThread().getName() + " try to capture object o");
+        synchronized (o) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread " + Thread.currentThread().getName() + " try to capture object o1");
+            synchronized (o1) {
+            }
+        }
+    }
+}
